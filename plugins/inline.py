415082cc22186a4fd2f8d78b35ec64a55bbcedaa
broglie4844 <83560557+broglie4844@users.noreply.github.com>
2022-07-14 10:33:13 -0700

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -69,12 +69,12 @@ async def answer(bot, query):
                            switch_pm_text=switch_pm_text,
                            switch_pm_parameter="okay")
 
-
+username = '@t48444844bot'
 def get_reply_markup(username, query):
     url = 't.me/share/url?url=' + quote(SHARE_BUTTON_TEXT.format(username=username))
     buttons = [
         [
-           InlineKeyboardButton('Search Again', switch_inline_current_chat='@t48444844bot '),
+           InlineKeyboardButton('Search Again', switch_inline_query_current_chat=query),
            InlineKeyboardButton('Share Our Bot', url=url),
         ],
     ]
